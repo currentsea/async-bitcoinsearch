fde812b6d96691a1cd053639d6f292f6e1ac8f86
Joe Bull <phoenix@atlantis.PK5001Z>
2016-10-10 13:51:48 -0700

    puadte async calls
    Signed-off-by: Joe Bull <phoenix@atlantis.PK5001Z>

--- okcoin.py
+++ okcoin.py
@@ -2,19 +2,17 @@
 
 import asyncio
 import websockets
-
+import json 
 OKCOIN_WEBSOCKETS_URL= "wss://real.okcoin.com:10440/websocket/okcoinapi"
 
 @asyncio.coroutine
-async def hello(): 
+async def processMarketData(channelSubscriptions): 
 	async with websockets.connect(OKCOIN_WEBSOCKETS_URL) as websocket:
 		try: 
-			substr = "{'event':'addChannel','channel':'ok_sub_spotusd_btc_ticker'}"
-			await websocket.send(substr)
-
+			for subscription in channelSubscriptions: 
+				print ("subscription: " + subscription)
+				await websocket.send("{'event':'addChannel','channel':'" + subscription + "'}") 
 			greeting = await websocket.recv()
-			print(greeting)
-			print ("connected to subscription")
 		finally: 
 			while True:
 				try:
@@ -23,13 +21,45 @@ async def hello():
 				except TypeError:
 					pass
 
-def consumer(message): 
-	print (message)
+# def getTickerMapping(): 
+# 	mapping = "okcoin_ticker": {
+# 		"properties": {
+# 			"uuid": { "type": "string", "index": "no"},
+# 			"date": {"type": "date"},
+# 			"last_price": {"type": "float"},
+# 			"timestamp": {"type": "string", "index": "no"},
+# 			"volume": {"type": "float"},
+# 			"high": {"type": "float"},
+# 			"ask": {"type": "float"},
+# 			"low": {"type": "float"},
+# 			"daily_change": {"type": "float"},
+# 			"daily_delta": {"type" : "float"},
+# 			"ask_volume": {"type": "float"},
+# 			"bid_volume": {"type": "float"},
+# 			"bid": {"type": "float"},
+# 			"currency_pair": {"type":"string"}
+# 		}
+# 	}
+# 	return mapping
+
+def getChannelDict(): 
+	channels  = ["ok_sub_spotusd_btc_ticker", "ok_sub_spotusd_ltc_ticker", "ok_sub_spotusd_btc_depth_20", "ok_sub_spotusd_ltc_depth_20", "ok_sub_spotusd_btc_depth_60", "ok_sub_spotusd_ltc_depth_60"]
+	return channels
+
+def consumer(marketData): 
+	jsonData = json.loads(marketData) 
+	print (jsonData)
+	# print (list(marketData)) 
+	# for blip in marketData: 
+	# 	print (blip["channel"])
+		# if blip["channel"] = "ok_sub_spotusd_ltc_ticker" 
+	# print (message)
 # async def processor():
 # 	async with websockets.connect(OKCOIN_WEBSOCKETS_URL) as websocket:
 # 		substr = "{'event':'addChannel','channel':'ok_sub_spotusd_btc_ticker'}"
 # 		await websocket.send(substr)
 
 # 		greeting = await websocket.recv()]
-
-asyncio.get_event_loop().run_until_complete(hello()) 	
+channels = getChannelDict()
+# print (dictShit) 
+asyncio.get_event_loop().run_until_complete(processMarketData(channels)) 	
