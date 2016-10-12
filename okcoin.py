3eda7662f6372e1d847770ae737e61436f02b94b
Joe Bull <phoenix@atlantis.local>
2016-10-12 05:28:56 -0700

    add depths for contracts
    Signed-off-by: Joe Bull <phoenix@atlantis.local>

--- okcoin.py
+++ okcoin.py
@@ -21,30 +21,12 @@ async def processMarketData(channelSubscriptions):
 				except TypeError:
 					pass
 
-# def getTickerMapping(): 
-# 	mapping = "okcoin_ticker": {
-# 		"properties": {
-# 			"uuid": { "type": "string", "index": "no"},
-# 			"date": {"type": "date"},
-# 			"last_price": {"type": "float"},
-# 			"timestamp": {"type": "string", "index": "no"},
-# 			"volume": {"type": "float"},
-# 			"high": {"type": "float"},
-# 			"ask": {"type": "float"},
-# 			"low": {"type": "float"},
-# 			"daily_change": {"type": "float"},
-# 			"daily_delta": {"type" : "float"},
-# 			"ask_volume": {"type": "float"},
-# 			"bid_volume": {"type": "float"},
-# 			"bid": {"type": "float"},
-# 			"currency_pair": {"type":"string"}
-# 		}
-# 	}
-# 	return mapping
-
 def getChannelDict(): 
-	channels  = ["ok_sub_spotusd_btc_ticker", "ok_sub_spotusd_ltc_ticker", "ok_sub_spotusd_btc_depth_20", "ok_sub_spotusd_ltc_depth_20", "ok_sub_spotusd_btc_depth_60", "ok_sub_spotusd_ltc_depth_60", "ok_sub_spotusd_btc_trades", "ok_sub_spotusd_ltc_trades", "ok_sub_spotusd_btc_kline_1min", "ok_sub_spotusd_btc_kline_3min", "ok_sub_spotusd_btc_kline_5min", "ok_sub_spotusd_btc_kline_15min", "ok_sub_spotusd_btc_kline_30min", "ok_sub_spotusd_btc_kline_1hour", "ok_sub_spotusd_btc_kline_2hour", "ok_sub_spotusd_btc_kline_4hour", "ok_sub_spotusd_btc_kline_6hour", "ok_sub_spotusd_btc_kline_12hour", "ok_sub_spotusd_btc_kline_day", "ok_sub_spotusd_btc_kline_3day", "ok_sub_spotusd_btc_kline_week", "ok_sub_spotusd_ltc_kline_1min", "ok_sub_spotusd_ltc_kline_3min", "ok_sub_spotusd_ltc_kline_5min", "ok_sub_spotusd_ltc_kline_15min", "ok_sub_spotusd_ltc_kline_30min", "ok_sub_spotusd_ltc_kline_1hour", "ok_sub_spotusd_ltc_kline_2hour", "ok_sub_spotusd_ltc_kline_4hour", "ok_sub_spotusd_ltc_kline_6hour", "ok_sub_spotusd_ltc_kline_12hour", "ok_sub_spotusd_ltc_kline_day", "ok_sub_spotusd_ltc_kline_3day", "ok_sub_spotusd_ltc_kline_week", "ok_sub_futureusd_btc_ticker_this_week","ok_sub_futureusd_btc_ticker_next_week", "ok_sub_futureusd_btc_ticker_quarter", "ok_sub_futureusd_ltc_ticker_this_week", "ok_sub_futureusd_ltc_ticker_next_week", "ok_sub_futureusd_ltc_ticker_quarter"]
-	return channels
+	channelDict = []
+	with open("channels.txt") as channels: 
+		for line in channels: 
+			channelDict.append(line.strip()) 
+	return channelDict
 
 def consumer(marketData): 
 	jsonData = json.loads(marketData) 
