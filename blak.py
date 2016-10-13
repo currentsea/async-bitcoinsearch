91b9465bfc4232e8f2baf5ed79d7f917ac250711
Joe Bull <phoenix@atlantis.local>
2016-10-13 10:50:35 -0700

    cleanup
    Signed-off-by: Joe Bull <phoenix@atlantis.local>

--- blak.py
+++
@@ -1,7 +0,0 @@
-websocket.send("{'event':'addChannel','channel':'ok_sub_futureusd_X_kline_Y_Z'}");
-① value of X is: btc, ltc
-② value of Y is: this_week, next_week, quarter
-③ value of Z is: 1min, 3min, 5min, 15min, 30min, 1hour, 2hour, 4hour, 6hour, 12hour, day, 3day, week
-
-
-
